1e59aeebc59624d96793bd2b06e52f85c9f6fde9
gioele-mari <gioele.mari@istitutoagnelli.it>
2024-06-25 22:16:54 +0200

    prova

---
+++ docs/assets/slidev/default-BQgf0Yqk.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Da8hOJj4.js";import{_ as r}from"../index-AT7xas6d.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
